39b6ac24fcb188b0ee0917d2a811dd4ba9f5de62
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-10-28 23:26:26 -0400

    added array of characters

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Array of Characters
+var symbolChar = ["!", "?", "@", "#", "$", "%", "&", "*", "<", ">", "+"];
+var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var allChar = [symbolChar , numberChar, upperChar, lowerChar];
+var randomPassword = [];
